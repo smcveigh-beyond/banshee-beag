f30475ffa59a18d7163fad043904236470bd37cb
ShoutingPenguin11 <sh0utingpenguin@gmail.com>
2024-10-31 15:59:49 +0000

    added sword and jump animation

--- app.js
+++ app.js
@@ -15,4 +15,44 @@ anime({
     direction: 'alternate', 
     loop: true,         
 });
+
+  document.addEventListener("keydown", function(event) {
+    // Check if the space bar (key code 32) is pressed
+    if (event.key === " " || event.keyCode === 32) {
+        event.preventDefault(); // Prevent default action (e.g., scrolling)
+        GhostJump();
+    }
+});
+
+const randomSwordAttack = function (){
+    anime({
+        targets: '#sword',
+        translateX: {
+          value: 1250,
+          duration: 1200
+        },
+        rotate: {
+          value: 360,
+          duration: 1800,
+          easing: 'easeInOutSine'
+        },
+        delay: 250 
+      });
+}
+
+const GhostJump = function(){
+    anime({
+        targets: '#banshee-beag, .thought-bubble',
+        translateY: [-400, 0],                
+        easing: 'easeInOutSine',      
+        duration: 800,                                
+    });
+
+}
+
+//random sword attack
+setTimeout(() => {
+    randomSwordAttack()
+}, 5000); 
+
   
