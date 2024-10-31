5a8bad0cb89e5b35c882a01597bc804744195085
Sarah Manoharan <smanoharan@beyond.com>
2024-10-31 13:18:44 +0000

    Remove constants from status tracker

--- StatusTracker.js
+++ StatusTracker.js
@@ -1,7 +1,3 @@
-let HEALTH = 100;
-let ENERGY = 100;
-let HUNGER = 100;
-let THIRST = 100;
 
 let statusList = document.getElementById("status-list");
 
@@ -14,12 +10,12 @@ const updateStatusBars = function(health, energy, hunger, thirst) {
     }
 
     for (let i = 0; i < statusList.childElementCount; i++) {
-        const text = Object.keys(statusMap)[i] + " : " + Object.values(statusMap)[i];
+        const text = Object.keys(statusMap)[i] + "  " + Object.values(statusMap)[i];
         const id = Object.keys(statusMap)[i].toLowerCase() + "-status";
         document.getElementById(id).textContent = text;
     } 
 }
 
-updateStatusBars(HEALTH, ENERGY, HUNGER, THIRST);
+updateStatusBars(100, 100, 100, 100);
 
 
