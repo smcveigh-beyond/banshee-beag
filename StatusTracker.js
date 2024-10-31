1c24135e315dd085bf91014c77d23c54eb9fd9c0
Sarah Manoharan <smanoharan@beyond.com>
2024-10-31 11:32:20 +0000

    Update status tracker

--- StatusTracker.js
+++ StatusTracker.js
@@ -5,8 +5,6 @@ let THIRST = 100;
 
 let statusList = document.getElementById("status-list");
 
-console.log(statusList);
-
 let statusMap = {
     "Health": HEALTH,
     "Energy": ENERGY,
@@ -14,21 +12,11 @@ let statusMap = {
     "Thirst": THIRST,
 }
 
-console.log(statusMap);
-
 for (let i = 0; i < statusList.childElementCount; i++) {
-    console.log("here");
     const element = document.createElement("p");
-
-    console.log(element);
-    const text = document.createTextNode(statusMap[i][0] + " :" + statusMap[i][1]);
-
-    element.appendChild(text);
-
-    console.log(element);
-
-    statusList[i].append(element);
-    console.log(statusList[i]);
+    const text = document.createTextNode(Object.keys(statusMap)[i] + " : " + Object.values(statusMap)[i]);
+    element.appendChild(text)
+    statusList.children[i].appendChild(element);
 }
 
 
