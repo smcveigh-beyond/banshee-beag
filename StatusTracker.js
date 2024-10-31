9584d4ba4951ac3b820f3cf791aa6dc68d43b739
Sarah Manoharan <smanoharan@beyond.com>
2024-10-31 14:10:03 +0000

    Updates coins

--- StatusTracker.js
+++ StatusTracker.js
@@ -3,6 +3,8 @@ let ENERGY = 100;
 let HUNGER = 100;
 let THIRST = 100;
 
+let COINS = 100;
+
 let statusList = document.getElementById("status-list");
 
 const updateStatusBars = function() {
@@ -13,13 +15,27 @@ const updateStatusBars = function() {
         "Thirst": THIRST,
     }
 
+    const values = Object.values(statusMap);
+    const keys = Object.keys(statusMap);
+
     for (let i = 0; i < Object.keys(statusMap).length; i++) {
-        const text = Object.values(statusMap)[i] + "%";
-        const id = Object.keys(statusMap)[i].toLowerCase() + "-status";
+        const text = values[i] + "%";
+        const id = keys[i].toLowerCase() + "-status";
         document.getElementById(id).textContent = text;
     } 
 }
 
+const updateCoins = function() {
+    document.getElementById("coins").textContent = COINS + " Coins";
+}
+
+
+updateCoins();
 updateStatusBars();
+
+
+
+
+
 
 
