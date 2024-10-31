d2b01098e707bd36ea7b1feba649e644854cbace
ShoutingPenguin11 <sh0utingpenguin@gmail.com>
2024-10-31 11:53:46 +0000

    animated svg progress

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 anime({
-    targets: '#banshee-beag',           // Target the ghost div
-    translateY: [0, 20],         // Move it 20px up and down
-    direction: 'alternate',      // Alternate between up and down
-    easing: 'easeInOutSine',     // Smooth easing for floating effect
-    duration: 2000,              // Duration of one up-and-down cycle
-    loop: true                   // Repeat forever for continuous floating
+    targets: '#banshee-beag',
+    translateY: [0, 20],          
+    direction: 'alternate',       
+    easing: 'easeInOutSine',      
+    duration: 2000,               
+    loop: true                    
+});
+
+anime({
+    targets: '.eye',
+    ry: [8, 0],         
+    easing: 'easeInOutQuad',
+    duration: 800,      
+    direction: 'alternate', 
+    loop: true,         
 });
   
