ef8abad5a3893cb6f607b4c2316bfa5169d2e410
ShoutingPenguin11 <sh0utingpenguin@gmail.com>
2024-10-31 14:40:09 +0000

    progress with ghost prompts

--- StatusTracker.js
+++ StatusTracker.js
@@ -7,6 +7,60 @@ let COINS = 100;
 
 let statusList = document.getElementById("status-list");
 
+let thoughtBubbleText = document.getElementById("thought-bubble-text");
+
+var prompts = {
+    health80: "I'm feeling okay.",
+    health60: "I'm starting to feel unwell.",
+    health40: "I'm starting to feel very unwell.",
+    health20: "I feel really weak!",
+
+    energy80: "I have enough energy to keep going.",
+    energy60: "I'm getting a bit tired.",
+    health40: "I'm getting very tired.",
+    energy20: "I'm exhausted!",
+
+    hunger80: "I could eat.",
+    hunger60: "I'm feeling hungry.",
+    hunger40: "I'm feeling very hungry.",
+    hunger20: "I'm very hungry!",
+
+    thirst80: "I'm feeling fine.",
+    thirst60: "I'm getting a bit thirsty.",
+    thirst40: "I'm getting very thirsty.",
+    thirst20: "I'm very thirsty!",
+
+    default: "Help!"
+};
+
+const meterCheckPrompt = function(meterValue, meterType){
+    alert("hello")
+    let promptsList = [prompts.health80, prompts.health60, prompts.health40, prompts.health20,
+        prompts.energy80, prompts.energy60, prompts.energy40, prompts.energy20,
+        prompts.hunger80, prompts.hunger60, prompts.hunger40, prompts.hunger20,
+        prompts.thirst80, prompts.thirst60, prompts.thirst40, prompts.thirst20, prompts.default
+    ];
+    let index = 0;
+    switch(meterType){
+        case "energy": index += 4; break;
+        case "hunger": index += 8;  break;
+        case "thirst": index += 16; break;
+    }
+
+    if(meterValue >= 80){
+        thoughtBubbleText.textContent = promptsList[index];
+    }
+    else if (meterValue > 60 && meterValue < 80) {
+        thoughtBubbleText.textContent = promptsList[index+1]; 
+    } else if (meterValue > 40 && meterValue <= 60) {
+        thoughtBubbleText.textContent = promptsList[index+2]; 
+    } else if (meterValue > 20 && meterValue <= 40) {
+        thoughtBubbleText.textContent = promptsList[index+3]; 
+    } else if (meterValue <= 20) {
+        thoughtBubbleText.textContent = promptsList[promptsList.length]; 
+    }
+}
+
 const updateStatusBars = function() {
     let statusMap = {
         "Health": HEALTH,
@@ -14,6 +68,11 @@ const updateStatusBars = function() {
         "Hunger": HUNGER,
         "Thirst": THIRST,
     }
+
+    meterCheckPrompt(health, "health");
+    meterCheckPrompt(energy, "energy");
+    meterCheckPrompt(hunger, "hunger");
+    meterCheckPrompt(thirst, "thirst");
 
     const values = Object.values(statusMap);
     const keys = Object.keys(statusMap);
@@ -23,6 +82,7 @@ const updateStatusBars = function() {
         const id = keys[i].toLowerCase() + "-status";
         document.getElementById(id).textContent = text;
     } 
+    
 }
 
 const updateCoins = function() {
@@ -32,10 +92,3 @@ const updateCoins = function() {
 
 updateCoins();
 updateStatusBars();
-
-
-
-
-
-
-
