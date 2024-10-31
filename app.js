1402d8567dc8f5f8c382cd887f12d7d5a4238c96
ShoutingPenguin11 <sh0utingpenguin@gmail.com>
2024-10-31 11:11:37 +0000

    animated banshee

---
+++ app.js
@@ -0,0 +1,9 @@
+anime({
+    targets: '#banshee-beag',           // Target the ghost div
+    translateY: [0, 20],         // Move it 20px up and down
+    direction: 'alternate',      // Alternate between up and down
+    easing: 'easeInOutSine',     // Smooth easing for floating effect
+    duration: 2000,              // Duration of one up-and-down cycle
+    loop: true                   // Repeat forever for continuous floating
+});
+  
